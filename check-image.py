662e32ceca8ee387283d213a90d86e2d655f6db8
Kris <kisokare@gmail.com>
2019-03-05 15:02:03 +0100

    update script to work properly
    also add possibility to exclude folders from checking

--- check-image.py
+++ check-image.py
@@ -1,10 +1,12 @@
-import requests # dependency
+import requests # dependency, needs to be installed
 import re
 import os
 import os.path
 import sys
 import shutil
+import math
 
+global cwd
 cwd = os.getcwd()
 
 missingImages = 0
@@ -18,8 +20,7 @@ allUnusedImages = []
 
 toCheckFileOrFolder = input("Do you want to check a file or a directory? (file/dir) ")
 
-import math
-
+# Function for size conversion of unused images
 def convert_size(size_bytes):
     if size_bytes == 0:
         return "0B"
@@ -51,17 +52,29 @@ def checkFile(file):
 
 
         # match any image
-        for line in f:
-            result = re.search(r'(http(s?):)?([/|.|\w|\s|-])*\.(?:jpg|gif|png)', line)
+        for num, line in enumerate(f, 1):
+            #result = re.findall(r'<!--[\s\S]*?-->|(?P<url>(http(s?):)?/?/.+?\.(jpg|gif|png))', line)
+            
+            
+            regexp = r'<!--[\s\S]*?-->|(?P<url>(http(s?):)?\/?\/?[^,;\'" \n\t>)(\\]+?\.(jpg|gif|png))'
+            result = [item[0] for item in re.findall(regexp, line) if item[0]]
             if result:
-                matchURL = line[result.start():result.end()]
+
+                num = str(num)
+                
+                matchURL = result[0]
                 if re.search(r'(http(s?):)', matchURL):
                     if checkUrl(matchURL) == False:
-                        allMissingImages.append(matchURL)
+                        allMissingImages.append(matchURL + " on line number " + num)
                 else:
-                    allImagesInFile.append(cwd + matchURL)
-                    if os.path.isfile(fileCheckingDir + matchURL) == False:
-                        allMissingImages.append(fileCheckingDir + matchURL)
+                    if matchURL[0] != "/":
+                        allImagesInFile.append(cwd + matchURL)
+                        if os.path.isfile(fileCheckingDir + matchURL) == False:
+                            allMissingImages.append(fileCheckingDir + matchURL + " on line number " + num + " in file " + fileCheckingDir + f.name)
+                    else:
+                        allImagesInFile.append(cwd + matchURL)
+                        if os.path.isfile(cwd + matchURL) == False:
+                            allMissingImages.append(cwd + matchURL + " on line number " + num + " in file " + fileCheckingDir + f.name)
 
         # check all existing images
         for path, subdirs, files in os.walk(fileCheckingDir+""):
@@ -69,15 +82,16 @@ def checkFile(file):
                 fullpath = os.path.join(path, name)
                 matches = re.search(r'([/|.|\w|\s|-])*\.(?:jpg|gif|png)',fullpath)
                 if matches:
-                    allImagesInDir.append(fullpath[matches.start():matches.end()])
-                    fstat = os.stat(fullpath[matches.start():matches.end()])
+                    if(any(substring in fullpath for substring in dirsToExclude) is not True):
+                        allImagesInDir.append(fullpath[matches.start():matches.end()])
+                        fstat = os.stat(fullpath[matches.start():matches.end()])
 
-                    global unusedImagesSize
-                    unusedImagesSize += fstat.st_size
+                        global unusedImagesSize
+                        unusedImagesSize += fstat.st_size
 
         # All images that are not used in directory
         global allUnusedImages
-        allUnusedImages = list(set(allImagesInDir) - set(allImagesInFile))
+        allUnusedImages = list(set(allImagesInDir) - set(allImagesInFile))    
     except: 
         pass
 
@@ -120,8 +134,10 @@ def tellUserStatus():
             image = str(image)
             image = image.replace(str(cwd), '')
             print(image)
+    else:
+        pass
     
-    saveToFile = input("Do you want to save the missing and unused images to a file? (y/n) ")
+    saveToFile = input("Do you want to save the missing and unused images to a .txt file? (y/n) ")
     if saveToFile == "y":
         file = open("IMAGE_REPORT.txt","w") 
  
@@ -134,22 +150,24 @@ def tellUserStatus():
         file.write("\n")
         file.write("\n")
         file.write("---------- Unused Images ----------\n")
-        for image in list(set(allMissingImages)):
+        for image in list(set(allUnusedImages)):
             cleanPath = re.sub(r'(?<!(https:))(?<!(http:))//', '/', image)
             cleanPath = cleanPath.replace(str(cwd), '')
             file.write(cleanPath+"\n")
         file.close()
+    else:
+        pass
     
-    moveToFolderForDeletion = input("Do you want to place all unused files inside a folder in the current directory? (y/n) ")
+    moveToFolderForDeletion = input("Do you want to place all unused files inside a folder (" + cwd +"/MOVED_FILES_FROM_CHECK_IMAGE_SCRIPT" + ")? (y/n) ")
     if moveToFolderForDeletion == "y":
         newpath = r""+ cwd +"/MOVED_FILES_FROM_CHECK_IMAGE_SCRIPT"
         if not os.path.exists(newpath):
             os.makedirs(newpath)
             for image in allUnusedImages:
                 shutil.move(str(image), cwd +"/MOVED_FILES_FROM_CHECK_IMAGE_SCRIPT/"+os.path.basename(image))
-        else:
-            print("Ok goodbye!")
-            sys.exit()
+    else:
+        print("Ok goodbye!")
+        sys.exit()
 
 
 if toCheckFileOrFolder == "file":
@@ -165,25 +183,42 @@ elif toCheckFileOrFolder == "dir":
 
     toCheckCurrOrCustom = input("Do you want to check the current directory or a custom directory? (curr/ABSOLUTE_PATH_TO_DIR) ")
     if toCheckCurrOrCustom != "curr":
-        cwd = toCheckCurrOrCustom
+        cwd = toCheckCurrOrCustom.strip()
     elif toCheckCurrOrCustom == "curr":
         cwd = cwd
     else:
-        print("ERROR: You didn't type curr or ABSOLUTE_PATH_TO_DIR. Please try again.")
+        print("ERROR: You didn't type 'curr' or 'ABSOLUTE_PATH_TO_DIR'. Please try again.")
         sys.exit()
 
-    for path, subdirs, files in os.walk(cwd+""):
+    global dirsToExclude
+    dirsToExclude = []
+    
+    toExcludeDir = input("Do you want to exclude any directories in (" + cwd + ") from being checked? (y/n) ")
+    if toExcludeDir == "y":
+
+        toStopExclusion = False
+        while toStopExclusion == False:
+            whichToExclude = input("Please type the pathname or exit if you're done. (RELATIVE_PATH_TO_DIR/exit) ")
+
+            if whichToExclude != "exit":
+                dirsToExclude.append(whichToExclude)
+                print("TO EXLCUDE: " + str(dirsToExclude))
+            else:
+                toStopExclusion = True
+
+    for path, subdirs, files in os.walk(str(cwd)):
         for name in files:
             fullpath = os.path.join(path, name)
-            matches = re.search(r'([/|.|\w|\s|-])*\.(?:html|php|js)',fullpath)
-            if matches:
-                filesToCheckInDir.append(fullpath[matches.start():matches.end()])
+            if(any(substring in fullpath for substring in dirsToExclude) is not True):
+                matches = re.search(r'([/|.|\w|\s|-])*\.(?:html|php|js|css)',fullpath)
+                if matches:
+                    filesToCheckInDir.append(fullpath[matches.start():matches.end()])
     
     if len(filesToCheckInDir) == 0:
-        print("No .html or .php files to check")
+        print("No .html, .css, .js or .php files to check in " + cwd)
     else:
         for fileToCheck in filesToCheckInDir:
             checkFile(fileToCheck)
         tellUserStatus()
 else:
-    print("ERROR: You didn't type file or dir. Please try again.")
+    print("ERROR: You didn't type 'file' or 'dir'. Please try again.")
